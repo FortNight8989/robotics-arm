4261b03255f56d2b25760be8d7343eff9cea729b
FortNight8989 <167669925+FortNight8989@users.noreply.github.com>
2024-04-21 13:23:14 +0700

    Add files via upload

---
+++ chuyen_vi.m
@@ -0,0 +1,5 @@
+ Rot_x(pi/2)*[2;3;4;1]
+ Rot_z(pi*35/180)*[5;3;8;1]
+ Rot_y(pi/2)*Trans(4,-3,7)*Rot_x(pi/2)*[2;3;5;1]
+ Rot_x(pi/2)*Trans(0,0,3)*Rot_z(pi*35/180)*Trans(0,5,0)*[1;5;4;1]
+ Rot_x(pi/2)*Trans(0,0,3)*Rot_z(pi*35/180)*Trans(0,5,0)*[1;5;4;1]
